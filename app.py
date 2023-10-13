e66c4f40195bf9805d1492c3002648230617c9f7
Phonex Chemutai <phinmaiyo@gmail.com>
2023-10-13 20:26:17 +0300

    commit

--- app.py
+++ app.py
@@ -7,58 +7,51 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # key lists
-expected_inputs = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
-       'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
-       'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
-       'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
-       'MonthlyCharges', 'TotalCharges']
+expected_inputs = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure','PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity','OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV','StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod','MonthlyCharges', 'TotalCharges']
 numerics = ['tenure', 'MonthlyCharges', 'TotalCharges']
-categoricals = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService',
-       'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup',
-       'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies',
-       'Contract', 'PaperlessBilling', 'PaymentMethod']
+categoricals = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService','MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup','DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies','Contract', 'PaperlessBilling', 'PaymentMethod']
 
 # Define helper functions
-# Function to load the toolkit
-def load_toolkit(file_path = r"src\Gradio_toolkit"):
+# Function to load the pipeline
+def load_pipeline(file_path = r"src\pipeline.pkl"):
     with open(file_path, "rb") as file:
-        toolkit = pickle.load(file)
-    return toolkit
-
-# Load_toolkit
-loaded_toolkit = load_toolkit(r"src\Gradio_toolkit")
+        pipeline = pickle.load(file)
+    return pipeline
 
 # Import the model
 model= RandomForestClassifier()
-model=joblib.load(r"src\customer_churn_model.pkl")
+model=joblib.load(r"src\rf_model.pkl")
 
-# Instantiate the items in the toolkit
-encoder= loaded_toolkit["encoder"]
-scaler= loaded_toolkit["scaler"]
+# Instantiate the pipeline
+pipeline= load_pipeline()
+
+scaler = None
+encoder = None
 
 # Function to process inputs and return prediction
 
 
-def predict_customer_attrition(model=model, scaler=scaler, encoder=encoder):
-    # Scale the numeric columns (you should use the scaler you have)
-    scaled_num = pd.DataFrame(scaler.transform(numerics), columns=numerics.columns, index=numerics.index)
-
-    # Encode the categorical columns 
-    encoded_cat = pd.DataFrame(encoder.transform(categoricals).toarray(), index=categoricals.index, columns=encoder.get_feature_names_out(categoricals.columns))
-
-    # Combine the scaled numeric and encoded categorical features for input data
-    input_data = pd.concat([scaled_num, encoded_cat], axis=1)
+def predict_customer_attrition(*args, pipeline=pipeline, model=model, scaler= scaler, encoder=encoder):
+    # Convert inputs into a dataframe
+    input_data = pd.DataFrame([args], columns=expected_inputs)
+    
 
     # Make the prediction 
-    model_output = model.predict(input_data)
+    model_output = pipeline.predict(input_data)
 
-    # Format the prediction result
-    return{"Prediction: Customer is likely to LEAVE": float(model_output[0]),
-           "Prediction: Customer is likely to STAY": 1 - float(model_output[0])}
+    if model_output == "Yes":
+        prediction = 1
+    else:
+        prediction = 0
 
 
-# Set up interface
-# Inputs
+    # Return the prediction
+    return {"Prediction: Customer is likely to LEAVE": prediction,
+            "Prediction: Customer is likely to STAY": 1 - prediction}
+
+
+    # Set up interface
+    # Inputs
 gender= gr.Dropdown(label = "What is the gender of the customer?", choices = ["Female", "Male"], value= "Male")
 SeniorCitizen= gr.Dropdown( label="Is the customer a senior citizen?", choices= ["No","Yes"], value="No")
 Partner= gr.Radio(label= "Does the customer have a partner?", choices= ["No", "Yes"], value="No")
@@ -80,18 +73,11 @@ MonthlyCharges= gr.Slider(label= "What is the monthly amount charged to the cust
 TotalCharges= gr.Slider(label= "What is the total amount charged to the customer?", minimum= 15, maximum= 9000, value= 220, interactive= True)
 
 
-
-# outputs
-iface = gr.Interface(
-    inputs= ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure','PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity','OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV','StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges'],
-    fn=predict_customer_attrition, 
-    outputs=gr.Label("Awaiting Submission..."),
-    title="Customer Attrition Prediction App",
-    description="This app was created by Santorini during our LP4 EDS",
-    live=True
-)
-
-iface.launch(inbrowser=True, show_error=True)
-
+# Create the Gradio interface
+gr.Interface(inputs=[gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges],
+             fn=predict_customer_attrition,
+             outputs= gr.Label("Awaiting Submission...."),
+             title="Customer Attrition Prediction App",
+             description="This app was created by Santorini during our LP4 EDS", live=True).launch(inbrowser=True, show_error=True)
 
 
